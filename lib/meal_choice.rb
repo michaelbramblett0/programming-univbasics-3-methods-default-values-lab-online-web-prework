24f8b9186226e68711879e92399a2a58e0a9d770
mystical-quotation-3156 <michaelbramblett0@gmail.com>
2019-09-30 07:02:39 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,4 @@
 # Your code here
+ibe '#meal_choice' do
+  it 'should default to meat for the protein' do
+    expect(meal_choice("broccoli", "macaroni")).to eq("A plate of meat with broccoli and macaroni.")
